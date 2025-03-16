5afe3fc0a25500f2529b184ebeb5552866236c24
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-03-16 08:45:17 +0400

    simple calculator (switch-Case)

--- ExerciesPart1/Program.cs
+++ ExerciesPart1/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace CourseExerciesPart1
+﻿namespace CourseExerciesCsharp
 {
     internal class Program
     {
@@ -163,40 +163,109 @@
 
             //-------------------------------------------------------
 
-            // Simple Calculator (9)
-            Console.WriteLine(" enter the first number:");
-            double num1 = double.Parse(Console.ReadLine());
+            //// Simple Calculator (9)
+            //Console.WriteLine(" enter the first number:");
+            //double num1 = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("enter an operator (+, -, *, /): ");
-            char op = Console.ReadLine()[0];
+            //Console.WriteLine("enter an operator (+, -, *, /): ");
+            //char op = Console.ReadLine()[0];
 
-            Console.WriteLine(" enter the second number:");
-            double num2 = double.Parse(Console.ReadLine());
+            //Console.WriteLine(" enter the second number:");
+            //double num2 = double.Parse(Console.ReadLine());
+
+            //double result = 0;
+
+            //if (op == '+')
+            //{
+            //    result = num1 + num2;
+            //}
+            //else if (op == '-')
+            //{
+            //    result = num1 - num2;
+            //}
+
+            //else if (op == '*')
+            //{
+            //    result = num1 * num2;
+            //}
+            //else if (op == '/')
+            //{
+            //    result = num1 / num2;
+            //}
+            //else
+            //{
+            //    Console.WriteLine("invalid operator");
+
+            //}
+            //----------------------------------------------
+            // ----------------Simple Calculator (Switch-Case)------------------
+
+            Console.Clear();
+            Console.WriteLine("Enter the operation number :\n1. Addition\n2. Subtraction ");
+            Console.WriteLine("3. Multiplication\n4. Division");
+
+            int OperationNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter first number :");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number :");
+            int num2 = int.Parse(Console.ReadLine());
 
             double result = 0;
 
-            if (op == '+')
-            {
-                result = num1 + num2;
-            }
-            else if (op == '-')
-            {
-                result = num1 - num2;
-            }
+            bool isActive = false;
 
-            else if (op == '*')
+            switch (OperationNumber)
             {
-                result = num1 * num2;
-            }
-            else if (op == '/')
-            {
-                result = num1 / num2;
-            }
-            else
-            {
-                Console.WriteLine("invalid operator");
+                case 1:
+                    result = num1 + num2;
+                    break;
+
+                case 2:
+                    result = num1 - num2;
+                    break;
+
+                case 3:
+                    result = num1 * num2;
+                    break;
+
+                case 4:
+
+                    if (num2 == 0)
+                    {
+                        Console.WriteLine("Division by zero not allowed");
+                        isActive = true;
+
+                    }
+                    else
+                    {
+                        result = num1 / num2;
+                    }
+
+                    break;
+
+                default:
+                    Console.WriteLine("Error! ");
+                    isActive = true;
+                    break;
 
             }
+            if (!isActive)
+            {
+                Console.WriteLine(result);
+            }
+            //--------------------------------------------------------
+            //----------------- Basic ATM System-----------------
+
+
         }
     }
-}
+
+
+
+
+
+        }
+    
+
