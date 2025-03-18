1abba3b7b6bddb9ed841f5053ab4d9ae8114c82e
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-03-18 07:48:41 +0400

    the largest Number

--- ExerciesPart1/Program.cs
+++ ExerciesPart1/Program.cs
@@ -32,16 +32,15 @@ namespace CourseExerciesCsharp
             //---------the largest Number (2)-------------------
             static void largestNumber()
             {
-                int num1, num2, num3;
-
+               
                 Console.WriteLine(" Enter the first number: ");
-                num1 = int.Parse(Console.ReadLine());
+               int  num1 = int.Parse(Console.ReadLine());
 
                 Console.WriteLine(" Enter the second number: ");
-                num2 = int.Parse(Console.ReadLine());
+               int  num2 = int.Parse(Console.ReadLine());
 
                 Console.WriteLine(" Enter the third number: ");
-                num3 = int.Parse(Console.ReadLine());
+               int  num3 = int.Parse(Console.ReadLine());
 
                 int largest = num1;
 
